a26285cce8586fc07892df97b1da662ee2e0ed7d
JEONG <dogug2865@gmail.com>
2020-11-24 00:09:23 +0900

    feat: lotto random

--- lotto/views.py
+++ lotto/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+
+number = int(input())
+
+for idx in range(number):
+    lotto_number = random.sample(range(1, 46), 6)
+    lotto_number.sort()
+    print("["+str((idx+1))+"번째"+"]", lotto_number)
+
+
+def home(request):
+    return render(request, 'lotto/home.html', lotto_number)
