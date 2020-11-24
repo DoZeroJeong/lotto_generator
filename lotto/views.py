c69622dac742d5bbe397dfd24ecbd33667f79693
JEONG <dogug2865@gmail.com>
2020-11-24 23:25:44 +0900

    feat: lotto random number generator

--- lotto/views.py
+++ lotto/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 import random
 # Create your views here.
 
-for idx in range(6):
-    lotto_number = random.sample(range(1, 46), 6)
-    lotto_number.sort()
-    print("["+str((idx+1))+"번째"+"]", lotto_number)
+
 
 
 def home(request):
-    return render(request, 'lotto/home.html', context={"lotto_number": lotto_number})
+    # generator = request.GET['generator']
+    lotto_list = []
+    for i in range(0, 5):
+        lotto_number = random.sample(range(1, 46), 6)
+        lotto_number.sort()
+        lotto_list.append(lotto_number)
+    return render(
+        request,
+        'lotto/home.html',
+        context={
+            "lotto_list": lotto_list,
+        }
+    )
