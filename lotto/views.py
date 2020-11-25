b5f267624594c71b7b6f33e496afa9e31cd3d741
JEONG <dogug2865@gmail.com>
2020-11-25 23:25:12 +0900

    refactor: html modified

--- lotto/views.py
+++ lotto/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 import random
-# Create your views here.
-
-
 
 
 def home(request):
-    # generator = request.GET['generator']
     lotto_list = []
     for i in range(0, 5):
         lotto_number = random.sample(range(1, 46), 6)
